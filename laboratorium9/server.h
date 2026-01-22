5c120c5a402f7609f71b4bf7cb3db04c580161ec
Hania <hania.27.kieszek@gmail.com>
2026-01-22 22:54:10 +0100

    Delete laboratorium9 directory

--- laboratorium9/server.h
+++
@@ -1,8 +0,0 @@
-#ifndef SERVER_H
-#define SERVER_H
-
-#include "shared.h"
-
-void start_server(Shared* s);
-
-#endif
--- laboratorium9/worker.h
+++
@@ -1,8 +0,0 @@
-#ifndef WORKER_H
-#define WORKER_H
-
-#include "shared.h"
-
-void* worker_thread(void* arg);
-
-#endif
